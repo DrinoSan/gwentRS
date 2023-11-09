e3781c7bdfc70cd693ec973031473da35b783801
DrinoSan <s.becirbegovic@gmail.com>
2023-11-09 20:54:48 +0100

    Added Game config Added config parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 mod oop;
+use oop::Game::ConfigErrorKind;
 use oop::Interface;
 
 fn main() {
@@ -13,8 +14,25 @@ fn main() {
 
     // oop for the module folder, First Interface for the file, second Interface for the struct
     // itself
-    let interface = Interface::Interface::new();
-    let game = oop::Game::Game::new(interface);
+    let io = Interface::Interface::new();
+    let mut game = oop::Game::Game::new(io);
+
+    let config_name: &str = &args[1];
+    let ret = match game.load_config(config_name) {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ConfigErrorKind::FsError => panic!("File {} not found please fix your path", &args[1]),
+            ConfigErrorKind::SerdeError => panic!("Json error valid is not valid"),
+        },
+    };
+
+    game.print_config();
+
+    // if ( !game.loadConfig( std::string( argv[ 1 ] ) ) )
+    // {
+    //     return Oop::RETURN_ERROR_BAD_CONFIG;
+    // }
+    // game.run();
 
     dbg!(args);
 }
--- src/oop/Game.rs
+++ src/oop/Game.rs
@@ -1,12 +1,61 @@
+use std::fs;
+
+use serde_json::{Error as SerdeError, Value};
+
 use super::Interface::Interface;
+
+#[derive(Debug)]
+pub enum ConfigErrors {
+    FsErrorOwn(std::io::Error),
+    SerdeErrorOwn(SerdeError),
+}
+
+impl From<SerdeError> for ConfigErrors {
+    fn from(error: SerdeError) -> Self {
+        ConfigErrors::SerdeErrorOwn(error)
+    }
+}
+
+#[derive(Debug)]
+pub enum ConfigErrorKind {
+    FsError,
+    SerdeError,
+}
+
+impl ConfigErrors {
+    pub fn kind(&self) -> ConfigErrorKind {
+        match self {
+            ConfigErrors::FsErrorOwn(_) => ConfigErrorKind::FsError,
+            ConfigErrors::SerdeErrorOwn(_) => ConfigErrorKind::SerdeError,
+        }
+    }
+}
 
 pub struct Game {
     io: Interface,
+    pub config: serde_json::Value,
 }
 
 impl Game {
     pub fn new(io: Interface) -> Self {
-        Game { io }
+        Game {
+            io,
+            config: Value::Null,
+        }
+    }
+
+    pub fn load_config(&mut self, file_name: &str) -> Result<(), ConfigErrors> {
+        let tmp_config = match fs::read_to_string(file_name) {
+            Ok(file) => file,
+            Err(error) => return Err(ConfigErrors::FsErrorOwn(error)),
+        };
+
+        self.config = serde_json::from_str(&tmp_config)?;
+
+        Ok(())
+    }
+
+    pub fn print_config(&self) {
+        println!("{}", serde_json::to_string_pretty(&self.config).unwrap());
     }
 }
-
